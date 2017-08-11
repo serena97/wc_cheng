c9fb72873d3df2adfcb37d241c87c4c1df0db301
Serena Chan <lok.chan@kcl.ac.uk>
2017-08-11 15:13:53 +0100

    added missing files

---
+++ js/about.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+//because jQuery cannot get
+$(".normal-c #about").click(function(){
+      console.log("okok")
+      //change background
+      $(".site-wrapper").css("background", "url(img/about.jpg) no-repeat center scroll")
+
+      var desc = "Currently studying in London college of fashion specialising in Fashion illustration. <br />"+
+                "Most of the work are portraits as I am really interested in experimenting different styles <br />"+
+                "to draw faces. Currently focusing on digitalising hand drawn work."
+
+      $("#content-placeholder .container").html(
+        '<div class="row animated fadeIn">'+
+          '<h4 style="font: 100 18px Helvetica, sans-serif;">'+
+          desc+
+          '</h4>'+
+        '</div>'
+
+      )
+      $(".footer").css("display", "block")
+});
+
+});
---
+++ js/illustration.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+//because jQuery cannot get
+$(".dropdown-content-c a").click(function(){
+      var linkName = $(this).attr('id')
+      var noOfImg = 0
+      switch(linkName) {
+      case "underground":
+          noOfImg = 9;
+          break;
+      case "playwithfire":
+          noOfImg = 2;
+          break;
+      default:
+          noOfImg = 0;
+      }
+      var pathname = "img/" + linkName
+      console.log(pathname)
+
+      //make background black
+      $(".site-wrapper").css("background", "black")
+      //disable footer
+      $(".footer").css("display", "none")
+      //replace container's contents with carousel, massive string is used instead of seperately $("#content-placeholder .container").html(somehtml) as divs are automatically added
+      var s1 = '<div id="slideshow" class="carousel slide" data-ride="carousel">' +
+               '<div class="carousel-inner">'
+
+      $("#content-placeholder .container").html()
+      var s2 = ""
+      //append
+      for(var i = 0; i < noOfImg; i++) {
+        var imgName = "img/" + linkName + "\/"+ i + ".jpg";
+        console.log(imgName)
+
+        if(i == 0) {
+          s2 +=  '<div class="item active">'+
+    	      	       '<img src='+'"'+imgName+'"'+' alt= "'+imgName+'"'+ '/>' +
+        		     '</div>'
+        } else {
+          s2 +=  '<div class="item">'+
+    	      	       '<img src='+'"'+imgName+'"'+' alt= "'+imgName+'"'+ '/>' +
+        		     '</div>'
+        }
+
+      }
+      console.log("finish looping")
+
+      $("#content-placeholder .container").html(s1 + s2 +
+        '</div>'+
+        '</div>'+
+        '</div>'+
+        '<div class="carousel-controls">'+
+          '<a class="left carousel-control" style="z-index: 1" href="#slideshow" data-slide="prev">' +
+          '<span class="glyphicon glyphicon-chevron-left"></span>' +
+          '</a>' +
+          '<a class="right carousel-control" style="z-index: 1" href="#slideshow" data-slide="next">'+
+          '<span class="glyphicon glyphicon-chevron-right"></span>'+
+          '</a>'+
+        '</div>'
+      )
+
+      $('.carousel').carousel({
+        interval: false
+      });
+});
+
+$("#watermark").click(function(){
+//hide <div class="container">
+  console.log("watermark clicked")
+  $(".site-wrapper").css("background", "url(../img/first-bg.png) no-repeat center scroll")
+  $("#content-placeholder .container").html(
+    '<div class="col-md-7 col-sm-7 christina animated fadeIn">CHRISTINA</div>'+
+    '<div class="col-md-5 col-sm-5 cheng animated fadeInCheng">CHENG</div>'
+  )
+  $(".footer").css("display", "none")
+
+});
+
+});
